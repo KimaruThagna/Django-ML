b30d80c05accaaa90644c4055ef6a5dfc43065ea
Kimaru thagana <macbuntu@localhost.localdomain>
2020-01-10 10:20:40 +0300

    setup classifier

--- MLAPI/ml_sample.py
+++ MLAPI/ml_sample.py
@@ -1,7 +1,8 @@
 from sklearn.feature_extraction.text import CountVectorizer
 import pickle
+from sklearn.linear_model import LogisticRegressionCV, LinearRegression
 '''
-is she mad? Finally, lets understand women
+is she mad? 
 1- Mad
 0- Not mad
 '''
@@ -13,3 +14,12 @@ responses = [
     ["im fine", 1],
     ["wow!!", 1],
 ]
+X, y = [], []
+for i in responses: # create feature and label lists
+    X.append( i[0] )
+    y.append( i[1] )
+
+vectorizer = CountVectorizer()
+X_vectorized = vectorizer.fit_transform(X) # transform features to allow ML model to evaluate text as vectors
+classifier = LogisticRegressionCV() # since its regression, closer to 1 means Mad, closer to 0 means not mad
+classifier.fit(X_vectorized, y) # fit classifier
