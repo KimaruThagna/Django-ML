ae83b797b626b3ce58f22def7a3c83bad4b6a45f
Kimaru thagana <macbuntu@localhost.localdomain>
2020-01-10 08:57:22 +0300

    setup relevant files in MLAPI app

--- MLAPI/views.py
+++ MLAPI/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+'''
+https://towardsdatascience.com/productionize-a-machine-learning-model-with-a-django-api-c774cb47698c
+https://www.kaggle.com/cedriclacrambe/keras-gpt-2-text-generation-tests
+https://www.learndatasci.com/tutorials/reinforcement-q-learning-scratch-python-openai-gym/
+https://www.freecodecamp.org/news/how-to-apply-reinforcement-learning-to-real-life-planning-problems-90f8fa3dc0c5/
+https://towardsdatascience.com/predicting-airbnb-prices-with-machine-learning-and-location-data-5c1e033d0a5a
+https://towardsdatascience.com/predicting-airbnb-prices-with-machine-learning-and-deep-learning-f46d44afb8a6
+https://link.medium.com/q5vnU2d962
+https://link.medium.com/zX4Bnsb962
+'''
