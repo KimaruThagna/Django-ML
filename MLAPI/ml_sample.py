d7ac058514f3a64c28dc8eca9c589f8691f7ee21
Kimaru thagana <macbuntu@localhost.localdomain>
2020-01-11 16:35:15 +0300

    perform test on logistic and linear regression

--- MLAPI/ml_sample.py
+++ MLAPI/ml_sample.py
@@ -1,18 +1,13 @@
 from sklearn.feature_extraction.text import CountVectorizer
 import pickle
-from sklearn.linear_model import LogisticRegressionCV, LinearRegression
+from sklearn.linear_model import LogisticRegression, LinearRegression
 '''
 is she mad? 
 1- Mad
 0- Not mad
 '''
 responses = [
-    ["fine do what you want", 1],
-    ["youre dumb", 0],
-    ["okay bye", 1],
-    ["Aki wewe", 0],
-    ["im fine", 1],
-    ["wow!!", 1],
+    ["fine do what you want", 1],    ["youre dumb", 0],    ["okay bye", 1],    ["Aki wewe", 0],    ["im fine", 1],    ["wow!!", 1],["gosh", 0],
 ]
 X, y = [], []
 for i in responses: # create feature and label lists
@@ -21,5 +16,12 @@ for i in responses: # create feature and label lists
 
 vectorizer = CountVectorizer()
 X_vectorized = vectorizer.fit_transform(X) # transform features to allow ML model to evaluate text as vectors
-classifier = LogisticRegressionCV() # since its regression, closer to 1 means Mad, closer to 0 means not mad
-classifier.fit(X_vectorized, y) # fit classifier
+classifier_1 = LogisticRegression() # since its regression, closer to 1 means Mad, closer to 0 means not mad
+classifier_2 = LinearRegression()
+classifier_1.fit(X_vectorized, y) # fit classifier
+classifier_2.fit(X_vectorized, y) # fit classifier
+# perform test
+tests = [vectorizer.transform(["fine"]), vectorizer.transform(["aki you"])]
+for item in tests:
+    print(f'Prediction1{classifier_1.predict(item)}')
+    print(f'Prediction2{classifier_2.predict(item)}')
