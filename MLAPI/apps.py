e1b487439d67378e40acfddf4b181be18532a0d2
Kimaru thagana <macbuntu@localhost.localdomain>
2020-01-11 16:44:39 +0300

    setup app.py by loading model from pickle file

--- MLAPI/apps.py
+++ MLAPI/apps.py
@@ -1,5 +1,15 @@
 from django.apps import AppConfig
-
+from django.conf import settings
+import os, pickle
 
 class MlapiConfig(AppConfig):
     name = 'MLAPI'
+    path = os.path.join(settings.ML_MODELS, 'pickled_models.p')
+
+    # load models into separate variables
+    # these will be accessible via this class
+    with open(path, 'rb') as pickled:
+        data = pickle.load(pickled)
+    regressor = data['regressor']
+    logistic = data['logistic']
+    vectorizer = data['vectorizer']
