5c275647465d3d797e81bf9a5a052db007af5b05
Kimaru thagana <macbuntu@localhost.localdomain>
2020-01-25 16:39:20 +0300

    multivariate imputation

--- MLAPI/imputation.py
+++ MLAPI/imputation.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
+from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import SimpleImputer
+from sklearn.impute._iterative import IterativeImputer
 # sample dataset Name and age of random college students
 df = pd.DataFrame([
                     ["Ann", 20],
@@ -18,3 +20,7 @@ df = pd.DataFrame([
 imp = SimpleImputer(strategy="most_frequent") # could be most_frequent, least_frequent, constant_value, mean, median
 print(imp.fit_transform(df))
 # multivariate imputation
+mi = IterativeImputer(max_iter=1000, random_state=0)
+mi.fit([[1, 0.5], [3, 1.5], [4, 2], [np.nan, 100], [7, np.nan]])
+X_test = [[np.nan, 100], [60, np.nan], [np.nan, 7]]
+print(np.round(mi.transform(X_test)))
