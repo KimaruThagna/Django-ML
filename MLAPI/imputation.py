e62b7df2b5d47575055ba73047765967be161f3d
Kimaru thagana <macbuntu@localhost.localdomain>
2020-01-25 16:15:47 +0300

    setup dataframe

--- MLAPI/imputation.py
+++ MLAPI/imputation.py
@@ -1,1 +1,15 @@
 import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+# sample dataset Name and age of random college students
+df = pd.DataFrame([
+                    ["Ann", 20],
+                    ["Mike",21],
+                    ["Alice",np.nan ],
+                    ["Jane",20],
+                    ["liam",19],
+                    ["Jimmy",20],
+                    ["Jane",20],
+                    ["Jimmy",np.nan],
+                    ["Helena",24],
+                    ], dtype='category')
